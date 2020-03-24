b5506b151b32134849a3ebd2975816f1488a6a92
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-24 16:11:21 -0300

    update incident lists
    + pagination
    + ong data on all incidents list

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -3,9 +3,21 @@ const connection = require('../database/connection');
 module.exports = {
   async index (request, response) {
     const ong_id = request.headers.authorization;
-
-    const incidents = await connection('incidents').where('ong_id', ong_id).select('*');
+    const { page = 1} = request.query;
     
+    const count = await connection('incidents')
+      .where('ong_id', ong_id)
+      .count()
+      .first();
+
+    const incidents = await connection('incidents')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .where('ong_id', ong_id)
+      .select('*');
+
+    response.header('X-Total-Count', count['count(*)'])
+
     return response.json({ incidents });
   }
 }

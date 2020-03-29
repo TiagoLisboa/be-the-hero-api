d013b2fd87d7abce87ccf83fff916ce5adff3f24
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-29 15:14:08 -0300

    validate ongs create
    using celebrate for validation

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,34 @@
-const express = require('express');
+const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 
-const OngController = require('./controllers/OngController');
-const IncidentController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController');
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.get("/ongs", OngController.index);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      whatsapp: Joi.string().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+    }),
+  }),
+  OngController.create
+);
 
-routes.post('/incidents', IncidentController.create);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.post("/incidents", IncidentController.create);
+routes.get("/incidents", IncidentController.index);
+routes.delete("/incidents/:id", IncidentController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.get("/profile", ProfileController.index);
 
-routes.post('/sessions', SessionController.create);
+routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
